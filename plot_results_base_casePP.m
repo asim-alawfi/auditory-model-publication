4d0cbf397106cb49ff49a86af8644c359c2fb3b9
asim-alawfi <asalawfi@hotmail.com>
2025-05-16 10:16:47 +0100

    test and it was ok.

--- plot_results_base_casePP.m
+++ plot_results_base_casePP.m
@@ -203,26 +203,50 @@ load('audi_simulations.mat')
 
 
 %%
+set_one_bif={'LineWidth',2,'Box','on','FontSize',11,'FontWeight','bold'};
 fiss=readtable('fission.csv'); fiss=fiss{:,:}; [~,idx]=sort(fiss(:,2)); fiss=fiss(idx,:);
 coher=readtable('coherence.csv'); coher=coher{:,:}; [~,idx]=sort(coher(:,2)); coher=coher(idx,:);
+%%
 figure(9)
 clf
 hold on
-set(gca, set_one_bif{:},'FontSize',10,'FontWeight','bold')
+set(gca, set_one_bif{:})
 pltcoh=plot3(1000./coher(:,2),2.^(coher(:,1)/12)-1,6*ones(length(coher)),'x','Color','k','LineWidth',1,'MarkerSize',7);
-pltfiss=plot3(1000./fiss(:,2),2.^(fiss(:,1)/12)-1,6*ones(length(fiss)),'x','Color','k','LineWidth',1,'MarkerSize',7);
+pltfiss=plot3(1000./fiss(:,2),2.^(fiss(:,1)/12)-1,6*ones(length(fiss)),'.','Color','k','LineWidth',1,'MarkerSize',10);
 x_text_fiss =7; 
 y_text_fiss =0.31; 
-text(x_text_fiss, y_text_fiss, 'fission', 'FontSize', 16, 'Color', 'black','Rotation', 0)
+%text(x_text_fiss, y_text_fiss, 'fission', 'FontSize', 14, 'Color', 'black','Rotation', 0)
 x_text_coh = 15; 
 y_text_coh= 0.327;
-text(x_text_coh,y_text_coh,'coherence', 'FontSize', 16, 'Color', 'black','Rotation',0)
+%text(x_text_coh,y_text_coh,'coherence', 'FontSize', 14, 'Color', 'black','Rotation',0)
+text(x_text_fiss, 0.4, '2 (bist)', 'FontSize', 12, 'Color', 'black','Rotation', 0)
+text(9, 0.1, '1 (int)', 'FontSize', 12, 'Color', 'black','Rotation', 0)
+text(12, 0.6, '3 (seg)', 'FontSize', 12, 'Color', 'black','Rotation', 0)
 
-vtext={'coher','fission'};
+vtext={'coherence','fission'};
 text_vec=[pltcoh(1);pltfiss(1)];
+legend(text_vec,vtext,'FontSize',12)
 ylim([0,1] )
+yticks([0,1])
 xlim([min(1000./coher(:,2)),max(1000./coher(:,2))])
-
+set(figure(9), 'Position',[10, 300, 450, 250])
+%%
+ft=13;
+xspace=linspace(-1,1,1e6);
+figure(700)
+clf 
+hold on
+plot(xspace,sig(xspace),'k','Linewidth',2)
+plot(xspace,SF(xspace),'-','Color',clrs2(1,:),'Linewidth',2)
+xline(0.5,'--','Color',0.7*[1 1 1],'LineWidth',2)
+xline(0.0,'--','Color',0.7*[1 1 1],'LineWidth',2)
+xticks([-1,-0.5,0,0.5,1])
+yticks([0,1])
+set(gca, set_one_bif{:})
+legend('$\theta=0$','$\theta=0.5$','interpreter','latex',FontSize=ft,FontWeight='normal')
+%ylabel('$S_{\theta}(x)$','interpreter','latex',FontSize=ft,FontWeight='normal')
+%xlabel('$x$','interpreter','latex',FontSize=ft,FontWeight='normal')
+set(figure(700), 'Position', [10, 300, 450, 250])
 %%
 ft=13;
 set_one_bif={'LineWidth',2,'Box','on','FontSize',10,'FontWeight','normal'};
@@ -343,3 +367,188 @@ xlim([0,0.053])
 xticks([0.026,0.053])
 ylim([0,1])
 yticks([0,0.5,1])
+%%
+%mbranch_df_wbifs
+
+% % tiledlayout(1,2)
+% % nexttile
+% % plot(pr_m(nunst_m==0),df_m(nunst_m==0),'k--','LineWidth',1);
+% figure(555)
+% clf;
+% for i=1:length(mbranch_df_wbifs.point) 
+%     subplot(1,2,1)
+%     hold on
+%     plot(pr_m(nunst_m==0),df_m(nunst_m==0),'k--','LineWidth',1);
+%     plot(pr_m(i),df_m(i),'k.','MarkerSize',15)
+%     hold on
+%     subplot(1,2,2)
+%     po=mbranch_df_wbifs.point(i);
+%     plot(po.mesh,po.profile(1:2,:),'LineWidth',2)
+%     yline(0.5,'k--','LineWidth',2)
+%     drawnow
+% end
+%%
+inds=find(diff(sign(pr_m-7)));
+figure(999)
+clf;
+subplot(1,3,1)
+    hold on
+    xline(7,'--','Color',0.5*[1 1 1],'LineWidth',2)
+
+    plot(pr_m(nunst_m==0),df_m(nunst_m==0),'k-','LineWidth',1.5);
+    plot(pr_m(inds(1)),df_m(inds(1)),'bx','MarkerSize',12,'LineWidth',1.5)
+        plot(pr_m(inds(2)),df_m(inds(2)),'rx','MarkerSize',12,'LineWidth',1.5)
+subplot(1,3,2)
+    hold on
+    po=mbranch_df_wbifs.point(inds(1));
+    plot(po.mesh,po.profile(1:2,:),'LineWidth',2)
+    plot(0.75,0.6,'bx','MarkerSize',12,'LineWidth',1.5)
+    yline(0.5,'k--','LineWidth',2)
+    subplot(1,3,3)
+    hold on
+    po2=mbranch_df_wbifs.point(inds(2));
+    plot(po2.mesh,po2.profile(1:2,:),'LineWidth',2)
+     plot(0.75,0.6,'rx','MarkerSize',12,'LineWidth',1.5)
+
+    yline(0.5,'k--','LineWidth',2)
+ %%
+ ft=13;
+ ftm=12;
+set_one_bif={'LineWidth',2,'Box','on','FontSize',10,'FontWeight','normal'};
+ figure(11)
+clf;
+tiledlayout(3,4,'TileSpacing','compact')
+nexttile([3 2])
+% %%%%%%%%%%%%%%%%%%%%% Exp range
+hold on;
+%%%%%%%%%%%%%%%%%% Bifurcation results
+plt1=plot(rp_symbk1(nunst_bk==0),df_symbk1(nunst_bk==0),'b-','color',clrs2(1,:),'LineWidth',4);
+%plot(rp_symbk1(nunst_bk>=1),df_symbk1(nunst_bk>=1),'k.','MarkerSize',8)
+plt_thsy=plot(rp_thta(dum==0),df_thta(dum==0),'m-','Linewidth',3);
+plt_thsy2=plot(rp_thta(dum>=1),df_thta(dum>=1),'','color',graycolor,'Linewidth',2);
+plt_thunsym=plot(pr_m(nunst_m==0),df_m(nunst_m==0),'k--',...
+pr_m(nunst_m>=1),df_m(nunst_m>=1),'--','Color','k','LineWidth',3);
+plt_thup=plot(rpp_x,rdff_x,'-.','Color',clrs2(5,:),'LineWidth',2.5);
+%%%%%%%% 
+df_ax_pr=arrayfun(@(x)x.parameter(in.df),branch_prlarge.point);  %df2_y=arrayfun(@(x)x.parameter(in.df),branch_prlarge2.point);
+
+[~,inds1]=min(abs(rpp_x-38));
+p_pr=br_stat_touch.point(inds1);
+plot(rp_thta(it),df_thta(it),'mx','MarkerSize',ftm,'LineWidth',2.5)
+plot(pr_m(inds(1)),df_m(inds(1)),'x','Color',clrs2(3,:),'MarkerSize',ftm,'LineWidth',2.5)
+plot(pr_m(inds(2)),df_m(inds(2)),'x','Color',clrs2(5,:),'MarkerSize',ftm,'LineWidth',2.5)
+plot(non_symper.point(it3).parameter(in.PR),non_symper.point(it3).parameter(in.df),'.',...
+    'Color',clrs2(7,:),'MarkerSize',25)%,'LineWidth',2)
+plot(p_pr.parameter(in.PR),p_pr.parameter(in.df),'x','color','k','MarkerSize',ftm,'LineWidth',2.5)
+[~,inds2]=min(abs(df_ax_pr-0.05));
+p_pr2=branch_prlarge.point(inds2);
+plot(p_pr2.parameter(in.PR),p_pr2.parameter(in.df),'.','color',0.45*[1 1 1],'MarkerSize',25)
+plt_text={'sym-breaking','touching \theta: stable sym','touching \theta: unstable sym ','touching \theta: stable non-sym','touching \theta from below: stable sym'};
+plt_vect=[plt1;plt_thsy;plt_thsy2;plt_thunsym;plt_thup];
+legend(plt_vect,plt_text,'FontSize',ft,'FontWeight','normal')
+%%%%%%%%%%%%
+
+set(gca, set_one_bif{:})
+text(5.5,0.1 ,'1 (int)', 'FontSize', 13, 'Color', 0.4*[1 1 1],'FontWeight','bold')
+text(7,0.25,'2 (bist)', 'FontSize', 13, 'Color', 0.4*[1 1 1],'Rotation',0,'FontWeight','bold')
+text(15,0.5,'3 (seg)', 'FontSize', 13, 'Color', 0.4*[1 1 1],'Rotation',0,'FontWeight','bold')
+text(32,0.05,'4 (sat)', 'FontSize', 13, 'Color', 0.4*[1 1 1],'Rotation',0,'FontWeight','bold')
+
+xlabel('$r_\mathrm{p}$','interpreter','latex',FontSize=17,FontWeight='bold')
+ylabel('$d_\mathrm{f}$','interpreter','latex',FontSize=17,FontWeight='bold')
+title('A','FontSize',ft,'FontWeight','normal')
+ylim([0,1] )
+yticks([0,0.5,1])
+xlim([0,40])
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+nexttile%([2 1])
+hold on
+plot(pt.mesh*pt.period,pt.profile(1:2,:),'LineWidth',3);
+yline(0.5,'k--','LineWidth',2,'FontSize',14)
+plot(0.05,0.9,'x','Color','m','MarkerSize',ftm,'LineWidth',2)
+set(gca, set_one_bif{:})
+title('B','FontSize',ft,'FontWeight','normal')
+%xlabel('time (s)',FontSize=ft,FontWeight='normal')
+
+xticks([0,pt.period/2,pt.period])
+xlim([0,pt.period])
+xtickformat('%3.2g')
+yticks([0,0.5,1])
+ylim([0,1])
+%grid on
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+nexttile%([2 1])
+hold on
+plot(po.mesh*po.period,po.profile(1:2,:),'LineWidth',3);
+yline(0.5,'k--','LineWidth',2,'FontSize',14)
+plot(0.25,0.9,'x','Color',clrs2(3,:),'MarkerSize',ftm,'LineWidth',2.5)
+set(gca, set_one_bif{:})
+%xlabel('time (s)',FontSize=ft,FontWeight='normal')
+title('C','FontSize',ft,'FontWeight','normal')
+%xlim([0,0.30])
+xlim([0,po.period])
+xticks([0,po.period/2,po.period])
+xtickformat('%3.2g')
+yticks([])
+ylim([0,1])
+nexttile%([2 1])
+hold on
+plot(po2.mesh*po2.period,po2.profile(1:2,:),'LineWidth',3);
+yline(0.5,'k--','LineWidth',2,'FontSize',14)
+plot(0.25,0.9,'x','Color',clrs2(5,:),'MarkerSize',ftm,'LineWidth',2.5)
+set(gca, set_one_bif{:})
+%xlabel('time (s)',FontSize=ft,FontWeight='normal')
+title('D','FontSize',ft,'FontWeight','normal')
+%xlim([0,0.30])
+xlim([0,po2.period])
+xticks([0,po2.period/2,po2.period])
+xtickformat('%3.2g')
+%xticks([0.15,0.30])
+yticks([0,0.5,1])
+ylim([0,1])
+nexttile
+hold on
+plot(p_pr.mesh*p_pr.period,p_pr.profile(1:2,:),'LineWidth',3)
+plot(0.04,0.9,'x','Color','k','MarkerSize',ftm,'LineWidth',2.5)%,'LineWidth',3)
+yline(0.5,'k--','LineWidth',2,'FontSize',14)
+set(gca, set_one_bif{:})
+%xlabel('time (s)',FontSize=ft,FontWeight='normal')
+title('E','FontSize',ft,'FontWeight','normal')
+xlim([0,p_pr.period])
+xticks([0,p_pr.period/2,p_pr.period])
+xtickformat('%3.2g')
+ylim([0,1])
+yticks([])
+nexttile%([2 1])
+hold on
+plot(pt3.mesh*pt3.period,pt3.profile(1:2,:),'LineWidth',3)
+plot(0.275,0.9,'.','Color',clrs2(7,:),'MarkerSize',25)%,'LineWidth',3)
+yline(0.5,'k--','LineWidth',2,'FontSize',14)
+set(gca, set_one_bif{:})
+xlabel('time (s)',FontSize=ft,FontWeight='normal')
+title('F','FontSize',ft,'FontWeight','normal')
+legend('$u_\mathrm{A}$','$u_\mathrm{B}$','Interpreter','latex','FontSize',ft)
+xlim([0,pt3.period])
+xticks([0,pt3.period/2,pt3.period])
+xtickformat('%3.2g')
+ylim([0,1])
+yticks([0,0.5,1])%grid on
+
+%%%
+df_ax_pr=arrayfun(@(x)x.parameter(in.df),branch_prlarge.point);  %df2_y=arrayfun(@(x)x.parameter(in.df),branch_prlarge2.point);
+
+[~,inds2]=min(abs(df_ax_pr-0.05));
+p_pr2=branch_prlarge.point(inds2);
+nexttile
+hold on
+plot(p_pr2.mesh*p_pr2.period,p_pr2.profile(1:2,:),'LineWidth',3)
+plot(0.04,0.7,'.','Color',0.45*[1 1 1],'MarkerSize',25)%,'LineWidth',3)
+yline(0.5,'k--','LineWidth',2,'FontSize',14)
+set(gca, set_one_bif{:})
+xlabel('time (s)',FontSize=ft,FontWeight='normal')
+title('G','FontSize',ft,'FontWeight','normal')
+xlim([0,p_pr2.period])
+xticks([0,p_pr2.period/2,p_pr2.period])
+xtickformat('%3.2g')
+ylim([0,1])
+yticks([])
